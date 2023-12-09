e615472c298aea512de8d40517734f7f5b713d5d
chatborrado <135921404+chatborrado@users.noreply.github.com>
2023-12-08 20:56:28 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-   apiKey: "AIzaSyA8RriQJJtfl4MxbKtSwl1uBxmH48clyF0",
-  authDomain: "chatborrado-89c70.firebaseapp.com",
-  projectId: "chatborrado-89c70",
-  storageBucket: "chatborrado-89c70.appspot.com",
-  messagingSenderId: "1002843686378",
-  appId: "1:1002843686378:web:6d2dfed2f6e207c82a5e93",
-  measurementId: "G-2GNRVBFBK2"
+   apiKey: "AIzaSyCpMZ9bom5LO9Ks5g_QmV263S95RMjtxxY",
+    authDomain: "chat5nv50.firebaseapp.com",
+    projectId: "chat5nv50",
+    storageBucket: "chat5nv50.appspot.com",
+    messagingSenderId: "842288443588",
+    appId: "1:842288443588:web:f4e0a1fa746a21c094d991",
+    measurementId: "G-3QQJMEFWD6"
     });
